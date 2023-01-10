620cf3be0c3458c9c800dc0b2ad79d88eca3869d
Iulian Caluian <iulian.coder@gmail.com>
2023-01-10 15:19:00 +0200

    Adding components

---
+++ ioComponentsConfigurator/Models/Component.cs
@@ -0,0 +1,19 @@
+﻿namespace ioComponentsConfigurator.Models
+{
+    public class Component
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int TemplateName { get; set; }
+        public List<ComponentPart>? Parts { get; set; }
+    }
+
+    public class ComponentPart
+    {
+        public string? Name { get; set; }
+        public ioInterfaceType? Type { get; set; }
+        public int DeviceId { get; set; }
+        public int Index { get; set; }
+        
+    }
+}
