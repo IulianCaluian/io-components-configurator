1b73afb1948b9db40f378a2261dc7f4f05309abd
Iulian Caluian <iulian.coder@gmail.com>
2023-01-12 22:39:15 +0200

    Adding a repository and AddComponent dialog

--- ioComponentsConfigurator/Program.cs
+++ ioComponentsConfigurator/Program.cs
@@ -1,4 +1,5 @@
 using ioComponentsConfigurator;
+using ioComponentsConfigurator.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddScoped<ComponentsRepository>();
 
 await builder.Build().RunAsync();
---
+++ ioComponentsConfigurator/Repositories/ComponentsRepository.cs
@@ -0,0 +1,67 @@
+﻿using ioComponentsConfigurator.Models;
+using System.IO;
+using System.Xml.Linq;
+
+namespace ioComponentsConfigurator.Repositories
+{
+    public class ComponentsRepository
+    {
+        private List<Component> _componentsList  = new List<Component>()
+        {
+            new Component()
+            {
+                Id = 1,
+                Name = "Bariera intrare",
+                Parts = new List<ComponentPart>()
+                {
+                    new ComponentPart() { Name = "Senzor", Type = ioInterfaceType.DigitalInputChannels, DeviceId = 4, Index = 0}
+                }
+            },
+            new Component()
+            {
+                Id = 2,
+                Name = "Usa",
+                Parts = new List<ComponentPart>()
+                {
+                    new ComponentPart() { Name = "Senzor", Type = ioInterfaceType.DigitalInputChannels, DeviceId = 4, Index = 0},
+                    new ComponentPart() { Name = "Lock", Type = ioInterfaceType.RelayChannels, DeviceId = 4, Index = 0}
+                }
+            }
+        };
+
+        private List<Template> _templatesList = new List<Template>()
+        {
+            new Template()
+            {
+                Id = 1, 
+                Name = "Bariera tip 1",
+                Properties = new List<TemplateProperty>()
+                {
+                    new TemplateProperty() { Name = "Senzor bariera" , InterfaceType = ioInterfaceType.DigitalInputChannels }
+                }
+            },
+            new Template()
+            {
+                Id = 2, 
+                Name = "Usa",
+                Properties = new List<TemplateProperty>()
+                {
+                    new TemplateProperty() { Name = "Contact" , InterfaceType = ioInterfaceType.DigitalInputChannels },
+                    new TemplateProperty() { Name = "Lock" , InterfaceType = ioInterfaceType.RelayChannels }
+                }
+            },
+            new Template() { Id = 3, Name = "Bariera tip 2" },
+            new Template() { Id = 4, Name = "Poarta mecanica" }
+            };
+
+        public List<Component> GetAllComponents()
+        {
+            return _componentsList;
+        }
+
+        public List<Template> GetAllTemplates()
+        {
+            return _templatesList;
+        }
+    }
+}
